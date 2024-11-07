783585c0484b5011cdfbfe377fd35ca2a7d9f92b
emmajane1313 <emma.jane@digitalax.xyz>
2024-11-07 16:01:52 -0500

    paquetes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,9 @@ import { Provider } from "react-redux";
 import "@rainbow-me/rainbowkit/styles.css";
 import { XMTPProvider } from "@xmtp/react-sdk";
 import { init } from "@airstack/airstack-react";
+import 'core-js/modules/es.array.map';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import moment from "moment";
 import {
   getDefaultWallets,
